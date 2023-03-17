9db35ad5c6aba59925c1024d13538164a711504d
yhcgg <yhcgg127@gmail.com>
2023-03-17 15:26:36 +0800

    add models

--- train.py
+++ train.py
@@ -33,7 +33,7 @@ def measurement(outputs, labels, smooth=1e-10):
             fn += 1
     return tp, tn, fp, fn
 
-def plot_accuracy(train_acc_list, val_acc_list):
+def plot_accuracy(train_acc_list, val_acc_list, savePath):
     # TODO plot training and testing accuracy curve
     plt.figure(figsize=(8, 4))
 
@@ -47,27 +47,29 @@ def plot_accuracy(train_acc_list, val_acc_list):
     plt.plot(val_acc_list)
     plt.title("Test_Accuracy")
 
+    plt.savefig(savePath + '/plot_accuracy.png')
     plt.show()
     pass
 
-def plot_f1_score(f1_score_list):
+def plot_f1_score(f1_score_list, savePath):
     # TODO plot testing f1 score curve
     plt.figure(figsize=(5, 3))
 
     plt.plot(f1_score_list)
     plt.title("Test_F1_score")
 
+    plt.savefig(savePath + '/f1_score.png')
     plt.show()
     pass
 
-def plot_confusion_matrix(confusion_matrix):
+def plot_confusion_matrix(confusion_matrix, savePath):
     # TODO plot confusion matrix
     plt.figure(figsize=(6, 4))
 
     sns.heatmap(confusion_matrix, annot=True, fmt=".0f",
                 xticklabels=['Predicted Normal', 'Predicted Pneumonia'],
                 yticklabels=['Actual Normal', 'Actual Pneumonia'])
-
+    plt.savefig(savePath + '/confusion_matrix.png')
     plt.show()
     pass
 
@@ -78,6 +80,7 @@ def train(device, train_loader, model, criterion, optimizer):
     val_acc_list = []
     f1_score_list = []
     best_c_matrix = []
+    best_epoch_info = {}
 
     for epoch in range(1, args.num_epochs+1):
 
@@ -110,7 +113,7 @@ def train(device, train_loader, model, criterion, optimizer):
             print(f'↳ Loss: {avg_loss}')
             print(f'↳ Training Acc.(%): {train_acc:.2f}%')
 
-        val_acc, f1_score, c_matrix = test(test_loader, model)
+        val_acc, f1_score, c_matrix, recall, precision = test(test_loader, model)
         train_acc_list.append(train_acc)
         val_acc_list.append(val_acc)
         f1_score_list.append(f1_score)
@@ -118,9 +121,12 @@ def train(device, train_loader, model, criterion, optimizer):
         if val_acc > best_acc:
             best_acc = val_acc
             best_c_matrix = c_matrix
+            best_epoch_info = {'epoch_num': epoch, 'train_loss': avg_loss, 'train_acc': train_acc,
+                               'recall': recall, 'precision': precision,
+                               'f1_score': f1_score, 'test_acc': val_acc}
             torch.save(model.state_dict(), 'best_model_weights.pt')
 
-    return train_acc_list, val_acc_list, f1_score_list, best_c_matrix
+    return train_acc_list, val_acc_list, f1_score_list, best_c_matrix, best_epoch_info
 
 def test(test_loader, model):
     val_acc = 0.0
@@ -147,10 +153,10 @@ def test(test_loader, model):
         recall = tp / (tp+fn)
         precision = tp / (tp+fp)
         f1_score = (2*tp) / (2*tp+fp+fn)
-        print (f'↳ Recall: {recall:.4f}, Precision: {precision:.4f}, F1-score: {f1_score:.4f}')
-        print (f'↳ Test Acc.(%): {val_acc:.2f}%')
+        print(f'↳ Recall: {recall:.4f}, Precision: {precision:.4f}, F1-score: {f1_score:.4f}')
+        print(f'↳ Test Acc.(%): {val_acc:.2f}%')
 
-    return val_acc, f1_score, c_matrix
+    return val_acc, f1_score, c_matrix, recall, precision
 
 if __name__ == '__main__':
     warnings.filterwarnings('ignore', category=DeprecationWarning)
@@ -160,10 +166,11 @@ if __name__ == '__main__':
 
     # for model
     parser.add_argument('--num_classes', type=int, required=False, default=2)
+    parser.add_argument('--model_name', type=str, required=False, default='resnet50')
 
     # for training
-    parser.add_argument('--num_epochs', type=int, required=False, default=2)
-    parser.add_argument('--batch_size', type=int, required=False, default=32)
+    parser.add_argument('--num_epochs', type=int, required=False, default=30)
+    parser.add_argument('--batch_size', type=int, required=False, default=128)
     parser.add_argument('--lr', type=float, default=1e-5)
     parser.add_argument('--wd', type=float, default=0.9)
 
@@ -182,20 +189,28 @@ if __name__ == '__main__':
 
     # set dataloader
     train_dataset = ImageFolder(root=os.path.join(args.dataset, 'train'),
-                                transform = transforms.Compose([transforms.Resize((args.resize, args.resize)),
+                                transform=transforms.Compose([transforms.Resize((args.resize, args.resize)),
                                                                 transforms.RandomRotation(args.degree, resample=False),
                                                                 transforms.ToTensor()]))
     test_dataset = ImageFolder(root=os.path.join(args.dataset, 'test'),
-                               transform = transforms.Compose([transforms.Resize((args.resize, args.resize)),
+                               transform=transforms.Compose([transforms.Resize((args.resize, args.resize)),
                                                                transforms.ToTensor()]))
 
     train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
     test_loader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False)
 
     # define model
-    model = models.resnet18(pretrained=True)
-    num_neurons = model.fc.in_features
-    model.fc = nn.Linear(num_neurons, args.num_classes)
+    if args.model_name == 'resnet18':
+        model = models.resnet18(pretrained=True)
+        num_neurons = model.fc.in_features  # 最後fc層的輸入
+        model.fc = nn.Linear(num_neurons, args.num_classes)
+    elif args.model_name == 'resnet50':
+        model = models.resnet50(pretrained=True)
+        num_neurons = model.fc.in_features
+        model.fc = nn.Linear(num_neurons, args.num_classes)
+    elif args.model_name == 'vgg16':
+        model = models.vgg16(pretrained=True)
+        model.classifier[-1] = nn.Linear(4096, args.num_classes)
     model = model.to(device)
 
     # define loss function, optimizer
@@ -204,9 +219,23 @@ if __name__ == '__main__':
     optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.wd)
 
     # training
-    train_acc_list, val_acc_list, f1_score_list, best_c_matrix = train(device, train_loader, model, criterion, optimizer)
+    train_acc_list, val_acc_list, f1_score_list, best_c_matrix, best_epoch_info = train(device, train_loader, model, criterion, optimizer)
+
+    # create plot save file
+    from datetime import datetime
+    path = datetime.now().strftime("%m%d_%H%M%S_result")
+    os.mkdir(path)
 
     # plot
-    plot_accuracy(train_acc_list, val_acc_list)
-    plot_f1_score(f1_score_list)
-    plot_confusion_matrix(best_c_matrix)
+    plot_accuracy(train_acc_list, val_acc_list, path)
+    plot_f1_score(f1_score_list, path)
+    plot_confusion_matrix(best_c_matrix, path)
+
+    # print best test accuracy
+    print("")
+    print("Best Test Accuracy -----------------------------------------------")
+    print(f'Epoch: {best_epoch_info["epoch_num"]}')
+    print(f'↳ Loss: {best_epoch_info["train_loss"]}')
+    print(f'↳ Training Acc.(%): {best_epoch_info["train_acc"]:.2f}%')
+    print(f'↳ Recall: {best_epoch_info["recall"]:.4f}, Precision: {best_epoch_info["precision"]:.4f}, F1-score: {best_epoch_info["f1_score"]:.4f}')
+    print(f'↳ Test Acc.(%): {best_epoch_info["test_acc"]:.2f}%')
